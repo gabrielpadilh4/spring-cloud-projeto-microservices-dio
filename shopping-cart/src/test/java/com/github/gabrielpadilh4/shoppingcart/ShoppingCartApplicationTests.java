3dec83c985b9dc80bec3dbb223c1fe25c217fa0f
Gabriel Padilha <gabrielpadilhasantos@gmail.com>
2021-07-02 16:00:50 -0300

    Initial Commit

---
+++ shopping-cart/src/test/java/com/github/gabrielpadilh4/shoppingcart/ShoppingCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.gabrielpadilh4.shoppingcart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
